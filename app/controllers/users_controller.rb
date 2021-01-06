f0c73d4cd79e15b05c4d7b202aa978043befcdd1
drewsview34 <drewsview34@outlook.com>
2021-01-06 12:03:50 -0800

    Added HTML to the Taco Creations Index view file and fixed errors in the post route in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,11 @@ class UsersController < ApplicationController
     post '/login' do
         # params looks like: {username: "username", password: "password"}
         # find the user
-        @user = user.find_by(username: params[:username])
+        @user = User.find_by(username: params[:username])
 
         # authenticate the user - verify the user is who they say they are
         # they have the credentials - email/password combo
-        if @user.authenticate{params[:password]}
+        if @user.authenticate(params[:password])
         # log the user in - create the user session
             session[:user_id] = @user.id
         # redirect to the user's landing page (show? index? dashboard?)
